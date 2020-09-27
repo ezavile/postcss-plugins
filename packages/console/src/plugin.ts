22c6fa4545318ee94202734ca48bb896886bef50
Andrey Sitnik <andrey@sitnik.ru>
2020-09-26 22:11:41 -0400

    fix(console): use faster API

--- packages/console/src/plugin.ts
+++ packages/console/src/plugin.ts
@@ -1,20 +1,25 @@
 import * as postcss from 'postcss';
 import MessageFactory from './MessageFactory';
 
+function log(atRule: postcss.AtRule) {
+  const message = MessageFactory.getMessage(atRule);
+  console.log(message.getFileName());
+  try {
+    console.log(message.output(atRule.params));
+    atRule.remove();
+  } catch (error) {
+    throw atRule.error(error);
+  }
+}
+
 const plugin = (): postcss.Plugin => {
   return {
     postcssPlugin: 'postcss-console',
-    Root(root: postcss.Root) {
-      root.walkAtRules(/console\w*/, (atRule) => {
-        const message = MessageFactory.getMessage(atRule);
-        console.log(message.getFileName());
-        try {
-          console.log(message.output(atRule.params));
-          atRule.remove();
-        } catch (error) {
-          throw atRule.error(error);
-        }
-      });
+    AtRule: {
+      'console.log': log,
+      'console.warn': log,
+      'console.error': log,
+      'console.assert': log,
     },
   };
 };
